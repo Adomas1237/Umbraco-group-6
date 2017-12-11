72218355235ce4efc93cb6e7a7db45ade1d045f6
Adomas1237 <31615497+Adomas1237@users.noreply.github.com>
2017-12-11 17:09:54 +0200

    some comments

---
+++ umbracoproject/Controllers/ScheduleApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+//not working
+namespace umbracoproject.Controllers
+{
+    public class ScheduleApiController : UmbracoApiController
+    {
+        public void AssignGroup(IMember member)
+        {
+            var MemberService = ApplicationContext.Services.MemberService;
+
+            MemberService.AssignRole(member.Id, "Active");
+
+            MemberService.Save(member);
+        }
+      
+    }
+ 
+}
---
+++ umbracoproject/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//not working
+namespace umbracoproject.Models
+{
+    public class Schedule
+    {
+       public string membid { get; set; }
+        public object sessions { get; set; }
+    }
+
+}
